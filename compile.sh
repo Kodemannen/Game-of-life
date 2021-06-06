9dac9244dc3cea7d79db4f518bf791279d53df09
kodemannen <samuelknudsen@hotmail.com>
2021-06-06 19:53:40 +0200

    Armadillo works, found out how to slice a vector

--- compile.sh
+++ compile.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+echo "----------------------------------------"
+echo "deleting old files:"
+echo "  "
 rm writefile > /dev/null 2>&1
-g++ main.cpp -o writefile
+
+echo "----------------------------------------"
+echo "compiling:"
+echo "  "
+g++ main.cpp -o writefile -std=c++11 -O2 -larmadillo
+
+        #
+        # -std is what standard library to use,
+        # -O2 is the level of optimization
+        # -l is library, I guess
+
+
+#g++ main.cpp -o writefile
+echo "----------------------------------------"
+echo "output:"
+echo "  "
 ./writefile 
--- main.cpp
+++ main.cpp
@@ -1,25 +1,80 @@
-
 #include <iostream>
-using namespace std;
+//#include <cmath>
+#include <armadillo>
 
-#include <iomanip>
-using std::setw;    // set width or something? 
-// setw(k) seems to insert k spaces
-//cout << "Element" << setw(13) << "Value" << endl;
+using namespace std;
+using namespace arma;
+
+
+// Create function for extracting a slice
+
+//#include <iomanip>
 
 int main () {
+
+    /*
+    We have a one dimensional grid. Each unit in the grid is 0 or 1,
+    visualized as black or white.
+
+    We iterate over the whole grid, and for each unit i we examine the triplet
+    that consists of unit_i and its nearest neighbours.
+
+    The number of possible microstates for this triplet is 2^N_x
+
+    The value of unit_i in the next generation is a function of this triplet.
+    */
  
-    int n[ 10 ]; 
+    arma_rng::set_seed(0); 
+
+    // 1-dimensional cellular automata thing
+    int N_x = 10;                   // number of units in the grid
+
+    int N_RULES = pow(2, N_x);      // pow exists in armadillo 
+    int RULES[ N_RULES ];
+
+    int T = 1;                   // number of timesteps/generations
+
+
+    // We can use uvec to slice!
+    rowvec pop = randi<rowvec>( N_x , distr_param(0, 9) );  
+    uvec indices = {1,2,7};
+    rowvec slice = pop.cols(indices);
     
-    for (int i=0; i<10; i++) {
-        n[i] = i + 100;
+    cout << pop << endl;
+    cout << slice << endl;
+    //cout << pop.submat(indices) << endl;
+    //cout << indices << endl;
+    //cout << pop << endl;
+
+    
+    
+
+    // Iterating over generations:
+    for (int t=0; t<T; t++) {
+
+        // Iterating over the units in the population:
+        for (int i=1; i<N_x-1; i++) {
+            
+            // not sure why, but it gives a warning unless I do this:
+            //
+            long unsigned a = i-1; long unsigned b = i; long unsigned c = i+1;
+
+            uvec indices = {a, b, c};
+
+            //uvec indices = {0,1,2};
+            rowvec triplet = pop.cols(indices);  
+            cout << triplet << endl;
+
+
+        }
+
     }
-
-    cout << "Element" << setw(13) << "Value" << endl;
-    cout << "Element" << "-------------" << "Value" << endl;
-    //for (int i=0; i<N; i++) {
-
-    //}
     
+
+    //cout << pop << endl;
+
+
+    cout << "Funker" << endl;
     return 0;
 }
+
