3699d14eef3eecabba3470f48dc9c1d977a2a742
kodemannen <samuelknudsen@hotmail.com>
2021-06-12 20:35:14 +0200

    Seems to work

--- main.cpp
+++ main.cpp
@@ -32,8 +32,11 @@ int main () {
                                                                 
     */
     // rng seed:
-    srand( (unsigned)time(NULL)); 
-    srand(2);   
+    //srand( (unsigned)time(NULL)); 
+    //srand(2);   
+    // Rng seed:
+    //arma::arma_rng::set_seed(2); 
+    arma::arma_rng::set_seed_random(); 
 
     // window size:
     int const WINDOW_WIDTH = 500; 
@@ -59,7 +62,7 @@ int main () {
 
     //----------------------------------------
     // Set screen position and size:
-
+    //----------------------------------------
     // (x, y), where (0,0) is top right corner
     window.setPosition(sf::Vector2i(0,20)); 
     window.setSize(sf::Vector2u(WINDOW_WIDTH, WINDOW_HEIGHT));
@@ -83,10 +86,31 @@ int main () {
     // initialize:
     //----------------------------------------
     // make matrix for the current state:
-    arma::mat state = arma::randi<arma::mat>( W, H , arma::distr_param(0, 1) );  
+    // 0s represent dead cells, 1s represent alive ones.
+    
+    // We let it be +2 larger in each dimension to add padding for periodic boundary 
+    //arma::mat state = arma::randi<arma::mat>( W+2, H+2 , arma::distr_param(0, 1) );  
+    arma::mat state = arma::zeros<arma::mat>( W+2, H+2 );  
+    int d = 100;
+    state(arma::span(50,50+d-1), arma::span(50,50+d-1)) = arma::ones<arma::mat>(d,d);
+
+
+    // Apply periodic boundary conditions:
+    arma::mat boundaryLeft = state.col(1);
+    arma::mat boundaryRight = state.col(W);
+    arma::mat boundaryTop = state.row(1);
+    arma::mat boundaryBottom = state.row(H);
+
+    state.col(0) = boundaryRight;
+    state.col(W+1) = boundaryLeft;
+    state.row(0) = boundaryBottom;
+    state.row(H+1) = boundaryTop;
+
 
     // make placeholder for the next generation:
-    arma::mat newState = arma::randi<arma::mat>( W, H , arma::distr_param(0, 1) );  
+    //arma::mat newState = arma::randi<arma::mat>( W+2, H+2 , arma::distr_param(0, 1) );  
+    arma::mat newState = state;
+
 
 
     // initialize pixels matrix:
@@ -184,29 +208,64 @@ int main () {
 
 arma::mat getNextGen(arma::mat state, arma::mat newState) {
     
-    int const H = state.n_rows;
-    int const W = state.n_cols;
+    int const H = state.n_rows-2;
+    int const W = state.n_cols-2;
 
     //arma::mat newState = arma::randi<arma::mat>( W, H , arma::distr_param(0, 1) );  
     //arma::mat newState = arma::diagmat(state);
     //arma:: mat neighbourhood(3,3);
 
-    for (int i=1; i<W-1; i++) {
-        for (int j=1; j<H-1; j++) {
-
-            if (i !=0 && j !=0) {
+    for (int i=1; i<W+1; i++) {
+        for (int j=1; j<H+1; j++) {
 
             auto current = state(i,j);
 
-            // Neighbourhood:
+            // Check neighbourhood:
             auto neighbourhood = state(arma::span(i-1,i+1), arma::span(j-1,j+1));
-            std::cout << neighbourhood << std::endl;
+            //std::cout << neighbourhood << std::endl;
 
-            break;
+
+            // count number of alive neighbours:
+            int aliveNeighbours = arma::accu(neighbourhood);
+
+
+            /* Rules:
+            1. Any live cell with to or three live neighbours survives. 
+            2. Any dead cell with three live neighbours becomes alive
+            3. All other live cells die the next generation, and all
+               other dead ones stay dead. 
+            */
+            
+
+
+            // Rule 1.  Any live cell with to or three live neighbours survives. 
+            if (current==1 && (aliveNeighbours==2 || aliveNeighbours==3)) {
+                newState(i,j) = 1;
             }
+            // Rule 2. Any dead cell with three live neighbours becomes alive
+            else if (current==0 && aliveNeighbours==3){
+                newState(i,j) = 1;
+            }
+            else {
+                newState(i,j) = 0;
+            }
+            
 
         }
+
     }
+
+    // Apply periodic boundary conditions: 
+    arma::mat boundaryLeft = newState.col(1);
+    arma::mat boundaryRight = newState.col(W);
+    arma::mat boundaryTop = newState.row(1);
+    arma::mat boundaryBottom = newState.row(H);
+
+    newState.col(0) = boundaryRight;
+    newState.col(W+1) = boundaryLeft;
+    newState.row(0) = boundaryBottom;
+    newState.row(H+1) = boundaryTop;
+
 
     return newState;
 }
@@ -221,19 +280,19 @@ int applyRules(arma::mat neighourhood) {
 
 sf::Uint8* armaMatrixToPixels(arma::mat state){ 
 
-    int const H = state.n_rows;
-    int const W = state.n_cols;
+    int const H = state.n_rows-2;
+    int const W = state.n_cols-2;
 
     // Make a matrix:
     sf::Uint8* pixels = new sf::Uint8[W*H*4];
     int val;
 
     int ind=0; 
-    for (register int i=0; i<H; i++) {
-        for (register int j=0; j<W; j++) {
+    for (register int i=1; i<H+1; i++) {
+        for (register int j=1; j<W+1; j++) {
 
-            //val = state(i,j)*255;
-            val = rand() % 255;
+            val = state(i,j)*255;
+            //val = rand() % 255;
             //std::cout << val << std::endl;
 
             // each pixel is represented by a set of four numbers
